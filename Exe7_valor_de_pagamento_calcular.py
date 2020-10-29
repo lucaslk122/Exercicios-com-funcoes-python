af1f2a0fba3b056b08b12f60198f8ce99aa4c26b
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-29 11:33:53 -0300

    Update Exe7_valor_de_pagamento_calcular.py

--- Exe7_valor_de_pagamento_calcular.py
+++ Exe7_valor_de_pagamento_calcular.py
@@ -1,15 +1,16 @@
 def valorPagamento(valor_prestação,dias_atraso):
     if dias_atraso == 0:
-        return "Sem atrasos, o valor a ser pago é o da propria prestação, ou seja, R$",valor_prestação
+        return "O valor a ser pago é o da propria prestação"
     else:
-        return valor_prestação + (valor_prestação*0.03) + (valor_prestação*dias_atraso*0.001)
+        pagar = valor_prestação + (valor_prestação*0.03) + (valor_prestação*dias_atraso*0.001)
+        return f"O valor a ser pago é R${pagar}"
 
-dasdasdasdasdasd
+
 while True:
     valor_prestação = float(input("Digite o valor da prestação  R$: "))
     dias_atraso = int(input("Digite os dias em atraso: "))
     resp = valorPagamento(valor_prestação,dias_atraso)
-    print(f"Valor a ser pago R${resp}")
+    print(resp)
     if valor_prestação != 0:
         continue
     else:
