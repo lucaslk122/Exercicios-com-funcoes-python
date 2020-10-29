7736f54ff5c78b27e0f9fc4adb3e1441cf5f2045
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-29 09:06:56 -0300

    dasda

---
+++ Exe2_Retorna_n_numeros_modificados.py
@@ -0,0 +1,12 @@
+def RetornaNnumeros(n):
+    for i in range(n + 1):
+        lista = ''
+        for j in range(i):
+            lista += str(i) + "\t"
+        print(lista)
+
+n = int(input("Digite n: "))
+RetornaNnumeros(n)
+
+
+
---
+++ Exe3_soma_3 numeros.py
@@ -0,0 +1,8 @@
+def SomaNumero(a,b,c):
+    s = a + b + c
+    return s
+
+a  = int(input("Digite um número: "))
+b  = int(input("Digite um número: "))
+c  = int(input("Digite um número: "))
+print(f"A soma de {a} + {b} + {c} é {SomaNumero(a,b,c)}")
